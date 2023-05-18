9088430c46bb6da36f18397ce3531036c0a5fff9
TusharPawar123 <tusharpawar82018@gmail.com>
2023-05-18 09:41:09 +0530

    new one

---
+++ src/add/Addition.java
@@ -0,0 +1,13 @@
+package add;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 3;
+int b = 6;
+int sum = (a+b);
+System.out.println(sum);
+	}
+
+}
