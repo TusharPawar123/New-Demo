68513e6ec8be54e89e0b10b979640ebecd2a87a8
TusharPawar123 <tusharpawar82018@gmail.com>
2023-05-18 09:46:46 +0530

    changing the code

--- src/add/Addition.java
+++ src/add/Addition.java
@@ -4,6 +4,9 @@ public class Addition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//add a comment
+		
+		//check it
 int a = 3;
 int b = 6;
 int sum = (a+b);
